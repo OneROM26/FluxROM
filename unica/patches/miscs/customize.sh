b01ee6951023fa7664787c5e4d41704eacd48aa1
Android-Artisan <romartisan2025@gmail.com>
2025-11-24 10:45:58 +0100

    Update build display ID format in customize.sh
    Removed the build display ID suffix from the ROM version string.

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -5,7 +5,7 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 ROM_STATUS=""
 $ROM_IS_OFFICIAL || ROM_STATUS=" UNOFFICIAL"
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
-SET_PROP "system" "ro.build.display.id" "MonsterROM$ROM_STATUS $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
+SET_PROP "system" "ro.build.display.id" "MonsterROM $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME"
 
 SET_PROP "system" "ro.extremerom.official" "$ROM_IS_OFFICIAL"
 SET_PROP "system" "ro.extremerom.version" "$ROM_VERSION"
