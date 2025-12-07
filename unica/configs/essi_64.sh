56e4c6c3e7146849e5ee7c3e9d95507c09b1b3ce
bieljose2012 <andressamontei@gmail.com>
2025-12-08 06:12:22 +0800

    Changing the source from the S25 Ultra to the S24+ source.

---
+++ unica/configs/essi_64.sh
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# MonsterROM configuration file for Exynos devices (essi/essi_64)
+
+# Galaxy S24+ (One UI 8.5)
+SOURCE_CODENAME="e2s"
+SOURCE_FIRMWARE="SM-S926B/EUX/359339510113441"
+SOURCE_EXTRA_FIRMWARES=()
+SOURCE_API_LEVEL=36.1
+SOURCE_PRODUCT_FIRST_API_LEVEL=34
+SOURCE_VNDK_VERSION="34"
+SOURCE_HAS_SYSTEM_EXT=false
+SOURCE_SUPER_GROUP_NAME="group_basic"
+
+# SEC Product Feature
+SOURCE_AUTO_BRIGHTNESS_TYPE="5"
+SOURCE_DVFS_CONFIG_NAME="dvfs_policy_default"
+SOURCE_NFC_CHIP_VENDOR="SLSI"
+SOURCE_FP_SENSOR_CONFIG="capacitive_powerkey_phone"
+SOURCE_HAS_MASS_CAMERA_APP=true
+SOURCE_HAS_QHD_DISPLAY=false
+SOURCE_DISPLAY_CUTOUT_TYPE="center"
+SOURCE_HFR_MODE="3"
+SOURCE_HFR_SUPPORTED_REFRESH_RATE="24,10,30,48,60,96,120"
+SOURCE_HFR_DEFAULT_REFRESH_RATE="120"
+SOURCE_HFR_SEAMLESS_BRT="89,91"
+SOURCE_HFR_SEAMLESS_LUX="260,2500"
+SOURCE_IS_ESIM_SUPPORTED=true
+SOURCE_SUPPORT_WIFI_7=false
+SOURCE_SUPPORT_HOTSPOT_DUALAP=true
+SOURCE_SUPPORT_HOTSPOT_WPA3=true
+SOURCE_SUPPORT_HOTSPOT_6GHZ=true
+SOURCE_SUPPORT_HOTSPOT_WIFI_6=true
+SOURCE_SUPPORT_HOTSPOT_ENHANCED_OPEN=true
+SOURCE_HAS_HW_MDNIE=true
+SOURCE_MDNIE_SUPPORTED_MODES="65303"
+SOURCE_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
+SOURCE_AUDIO_SUPPORT_ACH_RINGTONE=false
+SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION=true
+
